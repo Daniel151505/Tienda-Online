ac22ec3c996781b83d8bdd733b0e8c188a867dbb
Daniel151505 <daniel150520011@gmail.com>
2021-08-01 17:07:16 -0500

    Acomodando componentes

--- tienda-online/src/app/components/demo/demo.component.ts
+++ tienda-online/src/app/components/demo/demo.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DemoComponent implements OnInit {
 
+  title = 'tienda-online';
+
+  items = ['nicolas', 'luciana', 'sonia']
+
+  power = 10
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  // No se pueden iterar array
+  objeto = {};
+
+  // Agregar Item
+
+  addItem(){
+    this.items.push('nuevo item')
+  }
+
+  // Eliminar Item
+
+  deleteItem(index: number){
+    this.items.splice(index, 1)
+  }
+
+  clickProduct(id:number){
+    console.log('product')
+    console.log(id)
+  }
+
+
 
 }
