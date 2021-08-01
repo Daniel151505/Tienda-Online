aedf4b1a47b61aacc7d18503bd4e828769ec744f
Daniel151505 <daniel150520011@gmail.com>
2021-08-01 17:07:02 -0500

    Creando rutas en Angular

--- tienda-online/src/app/app-routing.module.ts
+++ tienda-online/src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './components/contact/contact.component';
+import { DemoComponent } from './components/demo/demo.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
 
-const routes: Routes = [];
+// Creando rutas en Angular
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'products',
+    component: ProductsComponent
+  },
+  {
+    path : 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'demo',
+    component: DemoComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
