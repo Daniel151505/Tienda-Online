a32141829cb04fddc7aaad24315b4a96f6e0851e
Daniel151505 <daniel150520011@gmail.com>
2021-08-01 23:18:11 -0500

    Usando routerLink y routerActive

--- tienda-online/src/app/app-routing.module.ts
+++ tienda-online/src/app/app-routing.module.ts
@@ -3,11 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './components/contact/contact.component';
 import { DemoComponent } from './components/demo/demo.component';
 import { HomeComponent } from './components/home/home.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { ProductsComponent } from './components/products/products.component';
 
 // Creando rutas en Angular
 
 const routes: Routes = [
+
+  // Redireccionamiento automatico
+  {
+    path:'',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
   {
     path: 'home',
     component: HomeComponent
@@ -23,6 +31,12 @@ const routes: Routes = [
   {
     path: 'demo',
     component: DemoComponent
+  },
+
+  // Redireccionamiento cuando la ruta esta mal
+  {
+    path: '**',
+    component: PageNotFoundComponent
   }
 
 ];
