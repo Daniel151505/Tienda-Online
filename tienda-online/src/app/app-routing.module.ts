345797f2c8d692e26f8299a2bdb6a1e5a88fc0f1
Daniel151505 <daniel150520011@gmail.com>
2021-08-07 13:40:00 -0500

    Vistas anidadas

--- tienda-online/src/app/app-routing.module.ts
+++ tienda-online/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './components/contact/contact.component';
 import { DemoComponent } from './components/demo/demo.component';
 import { HomeComponent } from './components/home/home.component';
+import { LayoutComponent } from './components/layout/layout.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { ProductsComponent } from './components/products/products.component';
@@ -11,15 +12,35 @@ import { ProductsComponent } from './components/products/products.component';
 
 const routes: Routes = [
 
-  // Redireccionamiento automatico
   {
     path:'',
-    redirectTo: '/home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    component: HomeComponent
+    component:LayoutComponent,
+    // Rutas hijos
+    children: [
+      // Redireccionamiento automatico
+      {
+        path:'',
+        redirectTo: '/home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'products',
+        component: ProductsComponent
+      },
+      //Redireccionamiento a producto de acuerdo al id
+      {
+        path: 'products/:id',
+        component: ProductDetailComponent
+      },
+      {
+        path : 'contact',
+        component: ContactComponent
+      }
+    ]
   },
   {
     path: 'products',
